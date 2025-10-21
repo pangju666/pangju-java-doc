40e9689929a54742ca3bce7f6b4ec3af95035b4d
胖橘 <nullptr255@outlook.com>
2025-10-22 04:14:52 +0800

    初始化仓库

---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,13 @@
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+plugin-prismjs@2._84c5d540a86cfe191fdc3e96f08e8b4a/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/index.js"
+import "E:/project/js/pangju-framework-doc/node_modules/.pnpm/@vuepress+highlighter-helpe_0aae4e83f09187d5c073912f45c053a9/node_modules/@vuepress/highlighter-helper/lib/client/styles/code-block-title.css"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
